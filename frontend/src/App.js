85702dc0384231af716147f92ba695187a1eba49
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-12-11 15:09:10 +0530

    Added new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import StepForm from './Components/fundRaiserForm';
+import SignUp from './Components/signUp';
+import LoginPage from './Components/Login';
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route element={<Home/>} path='/'/>
         <Route element={<StepForm/>} path='/raiseFund'/>
+        <Route element={<SignUp/>} path='/signUp'/>
+        <Route element={<LoginPage/>} path='/login'/>
       </Routes>
     </BrowserRouter>
   );
