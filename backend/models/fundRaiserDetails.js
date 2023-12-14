eec328f780f4946ece2a36732497994ea03d31c5
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-12-14 12:02:39 +0530

    Added fundraiser model

---
+++ backend/models/fundRaiserDetails.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const fundRaiserDetails = new mongoose.Schema({
+    fundRaiser: {
+        amount: String,
+        fundRaiserTitle: String,
+        fundRaiseFor: String,
+        employmentStatus: String,
+        image: String,
+        urEmail: String
+    },
+    patientDetails: {
+        Name: String,
+        age: String,
+        condition: String,
+        hospitalName: String,
+        city: String,
+    },
+    whyFundRaiser: {
+        story: String,
+        gdrive: String,
+    },
+    
+},
+    {
+        collection: 'fund-raiser',
+    }
+);
+const fundRaiser = mongoose.model("Fund raiser Info", fundRaiserDetails);
+module.exports = { fundRaiser }
