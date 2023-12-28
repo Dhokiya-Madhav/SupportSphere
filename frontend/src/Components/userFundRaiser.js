c656c34487a375ffdf459df14ecf8030c671f5ee
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-12-28 16:40:27 +0530

    Added code to delete the fundraiser

--- frontend/src/Components/userFundRaiser.js
+++ frontend/src/Components/userFundRaiser.js
@@ -8,7 +8,7 @@ export default function UserFundRaiser() {
     useEffect(() => {
         const fetchFundraisers = async () => {
             try {
-                const response = await fetch('http://localhost:5000/myfundraiser/'+sessionStorage.getItem('userEmail'));
+                const response = await fetch('http://localhost:5000/myfundraiser/' + sessionStorage.getItem('userEmail'));
                 if (!response.ok) {
                     throw new Error('Failed to fetch fundraisers');
                 }
@@ -22,42 +22,63 @@ export default function UserFundRaiser() {
         };
 
         fetchFundraisers();
-    }, []); 
+    }, []);
+
+    const handleDelete = async (id) => {
+        try {
+          const response = await fetch(`http://localhost:5000/delete-fundraiser/${id}`, {
+            method: 'DELETE',
+          });
+    
+          if (!response.ok) {
+            throw new Error('Failed to delete fundraiser');
+          }
+          setFundraisers((prevFundraisers) =>
+            prevFundraisers.filter((fundraiser) => fundraiser._id !== id)
+          );
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
     return (
-        <Card.Group className="mt-2 ms-1">
-            {fundraisers.map((fundraiser, index) => (
-                <Card key={index}>
-                    <Card.Content>
-                        <Image
-                            floated='right'
-                            size='mini'
-                            src={fundraiser.fundRaiser.image}
-                        />
-                        <Card.Header>{fundraiser.fundRaiser.fundRaiserTitle}</Card.Header>
-                        <Card.Meta>{fundraiser.patientDetails.Name}--{fundraiser.patientDetails.age} Years</Card.Meta>
-                        <Card.Description>
-                            Total Raised <br></br>
-                            Amount to be raised :- {fundraiser.fundRaiser.amount}
-                        </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <div className='ui two buttons'>
-                            <Link to='/fundraiser' state={{id:fundraiser._id}}>
-                            <Button basic color='green'>
-                                More Details
-                            </Button>
-                            </Link>
+        <div className="mx-auto mt-1 ms-4">
+            <Card.Group className="mt-2 ms-5">
+                {fundraisers.map((fundraiser, index) => (
+                    <Card key={index}>
+                        <Card.Content>
+                            <Image
+                                floated='right'
+                                size='mini'
+                                src={fundraiser.fundRaiser.image}
+                            />
+                            <Button floated="right" size="mini" onClick={() => handleDelete(fundraiser._id)}>Delete</Button>
+                            <Card.Header>{fundraiser.fundRaiser.fundRaiserTitle}</Card.Header>
+                            <Card.Meta>{fundraiser.patientDetails.Name}--{fundraiser.patientDetails.age} Years</Card.Meta>
+                            <Card.Description>
+                                Total Raised <br></br>
+                                Amount to be raised :- {fundraiser.fundRaiser.amount}
+                                <br></br>
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui three buttons'>
+                                <Link to='/fundraiser' state={{ id: fundraiser._id }}>
+                                    <Button basic color='green'>
+                                        More Details
+                                    </Button>
+                                </Link>
 
-                            <Link to='/update' state={{ id: fundraiser._id }}>
-                                <Button basic color='orange' className='ms-2'>
-                                    Edit
-                                </Button>
-                            </Link>
-                        </div>
-                    </Card.Content>
-                </Card>
-            ))}
-        </Card.Group>
+                                <Link to='/update' state={{ id: fundraiser._id }}>
+                                    <Button basic color='orange' className='ms-2'>
+                                        Edit
+                                    </Button>
+                                </Link>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                ))}
+            </Card.Group>
+        </div>
     );
 }
