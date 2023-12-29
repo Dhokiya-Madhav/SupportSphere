ed680ac3278c850b500cc6f19e71ce70acb0d498
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-12-29 12:04:43 +0530

    Added about us & footer component

---
+++ frontend/src/Components/aboutUs.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const MedicalCrowdfundingPlatform = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-8">
+          <div className="card" style={{ boxShadow: '0 4px 20px rgba(100,100,100)', border: '0px', padding: '20px' }}>
+            <div className="card-body">
+              <h2 className="card-title text-center mb-4">Welcome to Support-Sphere</h2>
+              <p className="card-text">
+                Your trusted medical crowdfunding platform dedicated to making a difference
+                in the lives of individuals facing healthcare challenges. At MedFunds,
+                we believe that everyone deserves access to quality healthcare, and we've
+                created a space where compassion meets technology. Our platform empowers
+                individuals and families to share their stories, connect with a global
+                community of supporters, and raise funds for medical expenses that might
+                otherwise be overwhelming. Whether it's covering the costs of surgeries,
+                treatments, or ongoing medical care, MedFunds provides a secure and transparent
+                space for patients to seek financial assistance. Join us in fostering a
+                community of hope, healing, and generosity as we work together to make
+                healthcare accessible to all. Together, we can turn dreams of recovery into reality.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MedicalCrowdfundingPlatform;
---
+++ frontend/src/Components/footer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Footer = () => {
+    return (
+        <>
+            <br></br><br></br>
+            <footer className="bg-dark text-light py-4 fixed-bottom">
+                <div className="container">
+                    <div className="row">
+                        {/* Navigation Links */}
+                        <div className="col-md-4">
+                            <h5>Navigation</h5>
+                            <ul className="list-unstyled">
+                                <li><Link to='http://localhost:3000/'>Home</Link></li>
+                                <li><Link to='http://localhost:3000/fr'>Browse Fundraiser</Link></li>
+                                <li><Link to='http://localhost:3000/about'>About Us</Link></li>
+                            </ul>
+                        </div>
+                        {/* Contact Information */}
+                        <div className="col-md-4">
+                            <h5>Contact</h5>
+                            <p><strong>Email:</strong> info@medicalcrowdfunding.com</p>
+                            <p><strong>Phone:</strong> +123-456-7890</p>
+                        </div>
+                        {/* Social Media Links */}
+                        <div className="col-md-4">
+                            <h5>Connect With Us</h5>
+                            <ul className="list-inline">
+                                <li className="list-inline-item"><a href="#" className="text-light"><i className="fab fa-facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-light"><i className="fab fa-twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-light"><i className="fab fa-instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-light"><i className="fab fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <hr className="my-2" />
+                    {/* Copyright Information */}
+                    <div className="row ">
+                        <div className="col-md-6">
+                            <p className="text-white">&copy; 2023 Medical Crowdfunding Platform. All rights reserved.</p>
+                        </div>
+                        <div className="col-md-6 text-right">
+                            <p className="text-white">Privacy Policy | Terms of Service</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
